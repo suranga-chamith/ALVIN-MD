d8a4003c9d5552681c8756c08afb6a129dc7b1b2
suranga-chamith <schamith036@gmail.com>
2024-12-19 00:33:24 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/oikfwc.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am Suranga*",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
+MODE: process.env.MODE || "public", //private or public
+AUTO_VOICE:"false" //true or false
+};
