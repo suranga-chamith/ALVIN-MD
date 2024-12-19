50da7d908cc7776472b69eb444f1481e559d8cc8
suranga-chamith <schamith036@gmail.com>
2024-12-19 22:05:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=j3ZB2TpK#mo3KHfL8ixrQUvy2kWDIZ6GmVI9Ke5EJt6TDJgEdcRE", //put your session id
+SESSION_ID: process.env.SESSION_ID || "", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/cx9d75.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am Suranga*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
